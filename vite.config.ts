1de3e7b3adc60c06cfea6e46026789bd2a579a6f
jstar_00 <wltjd6300@naver.com>
2025-11-14 20:58:06 +0900

    init: vite-plugin-svgr 설치 및 vite.config.ts 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: 'named', // SVG를 컴포넌트 이름으로 import (Named export)
+        ref: true, // JavaScript로 SVG 직접 접근 가능(useRef 등)
+        svgo: false, // 원본 SVG 파일을 그대로 import, 최적화 비활성화(원본 유지)
+        titleProp: true, // SVG에 <Logo title="" />와 같이 title 추가 가능(접근성 증대)
+      },
+      include: '**/*.svg',
+    }),
+  ],
 })
