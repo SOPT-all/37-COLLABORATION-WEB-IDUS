11a750b42b80f33762d2f7ad2311067840dbc205
odukong <ohbin1017@gmail.com>
2025-11-17 18:48:53 +0900

    init : storybook 초기 셋팅

---
+++ .storybook/preview.ts
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/react-vite";
+// (global.css) import 예정
+
+const preview: Preview = {
+  parameters: {
+    // 코드 변경 없이 storybook에서 동적으로 바꿔가며 인터렉션할 수 있도록 도와주는 기능
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    a11y: {
+      test: "todo",
+    },
+    tags: ["autodocs"], // 자동 문서화
+  },
+};
+
+export default preview;
