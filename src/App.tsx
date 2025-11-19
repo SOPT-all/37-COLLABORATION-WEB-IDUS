045951df1f0c3ea1d13930e776e3469b978d96c3
jstar_00 <wltjd6300@naver.com>
2025-11-19 18:40:38 +0900

    chore: 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
-import { typography } from "./shared/styles/typography.css";
-import * as styles from "./test.css";
 function App() {
   return (
-    <div>
-      <div className={`${typography.display()} ${styles.brandColor01}`}>
-        pretendard_SB_50_1
-      </div>
-      <div className={typography.heading1()}>pretendard_SB_32_1</div>
-      <div className={typography.heading2()}>pretendard_SB_24_1</div>
-      <div className={typography.heading3()}>pretendard_SB_18_1</div>
-      <div className={typography.body1()}>pretendard_SB_16_1</div>
-      <div className={typography.body2()}>pretendard_M_16_1</div>
-      <div className={typography.body3()}>pretendard_M_14_1</div>
-      <div className={typography.body4()}>pretendard_R_14_2</div>
-      <div className={typography.caption1()}>pretendard_M_12_1</div>
-      <div className={typography.caption2()}>pretendard_R_12_1</div>
-    </div>
+    <div></div>
   );
 }
 
--- src/test.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from "@vanilla-extract/css";
-import { color } from "./shared/styles/tokens/color.css";
-
-export const brandColor01 = style({
-  color: color.brandColor01,
-});
