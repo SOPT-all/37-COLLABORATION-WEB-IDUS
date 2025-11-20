b7a782bec2866304391eab17d87fe59b6b035637
jstar_00 <wltjd6300@naver.com>
2025-11-21 00:35:53 +0900

    feat: SmallButton 공통 컴포넌트 구현

---
+++ src/shared/components/button/large-button/large-button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as styles from './large-button.css'
+
+const LargeButton = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LargeButton
---
+++ src/shared/components/button/small-button/button.css.ts
@@ -0,0 +1,31 @@
+import { recipe } from "@vanilla-extract/recipes";
+import { typographyVars } from "@/shared/styles/typography.css";
+import { color } from "@/shared/styles/tokens/color.css";
+
+export const buttonStyles = recipe({
+  base: {
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    userSelect: "none",
+    whiteSpace: "nowrap",
+    borderRadius: "4px",
+    transition: "all 0.2s ease",
+    minWidth: "13.8rem",
+  },
+
+  variants: {
+    variant: {
+      gift: {
+        ...typographyVars.body2,
+        color: color.brand[100],
+        backgroundColor: color.brand[200],
+      },
+      purchase: {
+        ...typographyVars.body1,
+        color: color.white[100],
+        backgroundColor: color.brand[100],
+      },
+    },
+  },
+});
