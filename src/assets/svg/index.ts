0d3340ed241219d47a6ddaf0795a0dd3b2c94971
mimizae <jim400934@gmail.com>
2025-11-20 15:31:08 +0900

    feat: svg 파일들을 react 컴포넌트로 일괄 변환

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,30 @@
 // index.ts: SVG 파일들을 React 컴포넌트로 변환해 일괄적으로 관리
 // svg 폴더에 추가한 svg 이미지는 index.ts에서 아래와 같은 형식으로 export해 사용해주세요!
-export { default as IcnCredit } from "@svg/icnCredit.svg?react";
+
+
+// arrow
+export { default as ArrowLeft } from "@svg/arrow/back.svg";
+export { default as ArrowRight } from "@svg/arrow/right.svg";
+
+// chevron
+export { default as ChevronDown } from "@svg/chevron/down.svg";
+export { default as ChevronRightRounded } from "@svg/chevron/right-rounded.svg";
+export { default as ChevronRightSharp } from "@svg/chevron/right-sharp.svg";
+export { default as ChevronUp } from "@svg/chevron/up.svg";
+
+// heart
+export { default as MakerHeart } from "@svg/heart/maker.svg";
+export { default as BottomSheetHeart } from "@svg/heart/bottom-sheet.svg";
+
+// common
+export { default as Banner } from "@svg/banner.svg";
+export { default as Benefit } from "@svg/benefit.svg";
+export { default as ChatFloating } from "@svg/chat-floating.svg";
+export { default as Check } from "@svg/check.svg";
+export { default as Delete } from "@svg/delete.svg";
+export { default as Home } from "@svg/home.svg";
+export { default as Logo } from "@svg/logo.svg";
+export { default as Search } from "@svg/search.svg";
+export { default as Share } from "@svg/share.svg";
+export { default as ShoppingCart } from "@svg/shopping-cart.svg";
+export { default as Star } from "@svg/star.svg"
