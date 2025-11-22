f66346b1618100073aa2de726be4497b64df8e85
jstar_00 <wltjd6300@naver.com>
2025-11-22 15:09:27 +0900

    feat: BottomSheet에 React Portal 적용

--- src/shared/components/bottom-sheet/bottom-sheet.tsx
+++ src/shared/components/bottom-sheet/bottom-sheet.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { createPortal } from "react-dom";
 import * as styles from "./bottom-sheet.css";
 
 interface Props {
@@ -10,14 +11,15 @@ interface Props {
 const BottomSheet = ({ isOpen, onClose, children }: Props) => {
   if (!isOpen) return null;
 
-  return (
+  return createPortal(
     // TODO: 접근성 고려
     <div className={styles.overlay}>
       <div className={styles.backdrop} onClick={onClose} />
       <div className={styles.sheet}>
         <div className={styles.content}>{children}</div>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 };
 
