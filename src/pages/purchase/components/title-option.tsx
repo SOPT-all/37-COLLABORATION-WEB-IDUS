4b3214baaeadd67868b21ac51662c3e88c0d6ea8
jstar_00 <wltjd6300@naver.com>
2025-11-26 22:10:19 +0900

    feat:TextField 컴포넌트에 stopPropagation 적용

--- src/pages/purchase/components/title-option.tsx
+++ src/pages/purchase/components/title-option.tsx
@@ -1,6 +1,7 @@
 import { Check } from "@/assets/svg";
 import * as styles from "./title-option.css";
 import TextField from "@/shared/components/input/text-field";
+import type React from "react";
 
 interface Props {
   title: string;
@@ -17,13 +18,21 @@ const TitleOption = ({
   isExpanded,
   onSelect,
 }: Props) => {
+  const handleTextFieldClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return (
     <div className={styles.container({ isExpanded })} onClick={onSelect}>
       <div className={styles.titleContainer}>
         <p className={styles.titleText({ isExpanded })}>{title}</p>
         {isExpanded && <Check />}
       </div>
-      {isExpanded && <TextField label={label} placeholder={placeholder} />}
+      {isExpanded && (
+        <div onClick={handleTextFieldClick}>
+          <TextField label={label} placeholder={placeholder} />
+        </div>
+      )}
     </div>
   );
 };
