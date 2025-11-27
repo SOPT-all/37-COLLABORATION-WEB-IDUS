1cfac2f652ceb78b51c81319df52f8e0eaae0afc
odukong <ohbin1017@gmail.com>
2025-11-27 18:54:04 +0900

    fix: endPoint url 경로의 slash 제거

--- src/apis/constants/api-endpoints.ts
+++ src/apis/constants/api-endpoints.ts
@@ -4,7 +4,7 @@ export const API_ENDPOINTS = {
     AUTHOR_LIKES: (authorId: number) => `/authors/${authorId}/likes`,
   },
   PRODUCTS: {
-    PRODUCT_INFO: (productId: number) => `/products/${productId}`,
+    PRODUCT_INFO: (productId: number) => `products/${productId}`,
     PRODUCT_REVIEW: (productId: number) => `/products/${productId}/reviews`,
     PRODUCT_LIKES: (productId: number) => `/products/${productId}/likes`,
   },
